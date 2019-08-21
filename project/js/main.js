7e83da14498663d6fd024b7382289fabdc45a6c3
Noam Kahn <nkahn@bosmmfy33vjj1wt.hubspot.local>
2019-08-21 18:24:44 -0400

    gus is cool

--- project/js/main.js
+++ project/js/main.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
 })
 
 function getWeather() { 
-var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&APPID"+apiKey;
+var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&APPID"+apiKey;
 
 $.ajax(url,{success: function(data){
-  console.log(data);
+  $(".city").text(data.name);
+  $(".temp").text(data.maintemp);
 }})
 }
