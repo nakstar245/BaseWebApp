fcebcee24807b2a6480a474ccdbb9d0d5eee4870
Noam Kahn <nkahn@bosmmfy33vjj1wt.hubspot.local>
2019-08-21 18:18:06 -0400

    second chance

--- project/js/main.js
+++ project/js/main.js
@@ -8,13 +8,4 @@ var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&APPID"+apiKe
 $.ajax(url,{success: function(data){
   console.log(data);
 }})
-
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("Enjoy these pretty pup. How many can you get?!");
-
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
 }
